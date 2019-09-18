ac659845f0dea31363060fdbf4262b98807db928
AhmedOmi <54010086+AhmedOmi@users.noreply.github.com>
2019-09-18 13:18:51 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main()
-{
-  puts ("\"Programming is like building a multilingual puzzle");
-  return 0;
- }
+{puts ("\"Programming is like building a multilingual puzzle");
+return 0;}
