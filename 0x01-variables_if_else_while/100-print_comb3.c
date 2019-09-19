f532fe9985a392f8f707e7b69d5581b84ef8d710
AhmedOmi <ahmedomarmiledi@gmail.com>
2019-09-19 19:54:16 +0000

    100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - 10
+ * Ahmed omar miladi
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+int i;
+int j;
+for (i = 48; i <= 57; i++)
+for (j = 48; j <= 57; j++)
+if (i < j)
+{
+putchar(i);
+putchar(j);
+if (i != 56 || j != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
