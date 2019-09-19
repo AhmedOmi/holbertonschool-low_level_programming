d4201a69172cbd71153196217a299f5fa2036c5c
AhmedOmi <ahmedomarmiledi@gmail.com>
2019-09-19 15:41:38 +0000

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - 6
+ * Ahmed Omar Miladi
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
