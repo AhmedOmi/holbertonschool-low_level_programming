0e40742a0b9f81dea8ad4de5fa1b9cc0d83a6ba4
AhmedOmi <ahmedomarmiledi@gmail.com>
2019-09-19 15:53:06 +0000

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - 7
+ * Ahmed omar miladi
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{char i;
+for (i = 'z'; i >= 'a'; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
