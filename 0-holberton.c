67a297c7b8862ebb5a4bc6422cedc971c3bb247b
AhmedOmi <ahmedomarmiledi@gmail.com>
2019-09-24 10:21:56 +0000

    holberton

---
+++ 0-holberton.c
@@ -0,0 +1,15 @@
+#include"holberton.h"
+/**
+ * main - Entry point
+ * 0
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+char h[20] = "Holberton";
+for (i = 0; h[i] ; i++)
+putchar(h[i]);
+putchar('\n');
+return (0);
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# holbertonschool-low_level_programming
+readme all loops functions 
