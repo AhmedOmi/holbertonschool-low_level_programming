361a7148c86f318ceecd3ed12d88fded9268db50
AhmedOmi <ahmedomarmiledi@gmail.com>
2019-09-24 14:22:50 +0000

    commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,46 +1,50 @@
 #include "holberton.h"
 /**
- * print_times_table - entry point
- * @n: n times
- * Return: Void
- */
+* print_times_table - print n times the tables
+* @n: where start table
+*
+*Return: nothing
+*/
 void print_times_table(int n)
 {
-int i, j;
-if (n <= 15)
-{
-for (i = 0; i <= n; i++)
-{
-_putchar('0');
-_putchar(',');
-for (j = 1; j <= n; j++)
-{
-_putchar(' ');
-if (i * j < 10)
-{
-_putchar(' ');
-_putchar(' ');
-_putchar(i * j  + '0');
-}
-else if  (i * j >= 10 && i * j < 100)
-{
-_putchar(' ');
-_putchar(i * j / 10 + '0');
-_putchar(i * j % 10 + '0');
-}
-else
-{
-_putchar(i * j / 100 + '0');
-_putchar((i * j / 10) % 10 + '0');
-_putchar(i * j % 10 + '0');
-}
+int r, c;
+int h, d, u, mul;
 
-if (j < n)
+if (n >= 0 && n <= 15)
+{
+for (r = 0; r <= n; r++)
+{
+for (c = 0; c <= n; c++)
+{
+mul = r * c;
+h = mul / 100;
+d = mul / 10 % 10;
+u = mul % 10;
+if (h == 0 && c != 0)
+{
+_putchar(' ');
+if (d == 0)
+_putchar(' ');
+else
+_putchar(d + '0');
+_putchar(u + '0');
+}
+else if (h != 0)
+{
+_putchar(h + '0');
+_putchar(d + '0');
+_putchar(u + '0');
+}
+if (c == 0)
+_putchar('0');
+if (c != n)
 {
 _putchar(',');
+_putchar(' ');
 }
 }
 _putchar('\n');
+c = 0;
 }
 }
 }
