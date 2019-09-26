dea1c14e7545fc1452be64d437e5da9872f8c047
AhmedOmi <ahmedomarmiledi@gmail.com>
2019-09-26 21:37:18 +0000

    commit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -3,26 +3,24 @@
 /**
  * print_number - prints
  * @n: integer
- * Return: void
+ * Return:  void
  */
 void print_number(int n)
 {
 int i = 1;
-int s;
 if (n < 0)
 {
 n = -n;
 _putchar('-');
 }
 while (i < n)
-i = i * 10;
+i *= 10;
 if (i > 10)
-i = i / 10;
+i /= 10;
 while (i)
 {
-s = n / i;
-_putchar(s + '0');
-n = n % i;
-i = i / 10;
+_putchar(n / i + '0');
+n %= i;
+i /= 10;
 }
 }
