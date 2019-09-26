a19459d0812166b16798a5ad6ade6951d5bf5c03
AhmedOmi <ahmedomarmiledi@gmail.com>
2019-09-26 20:55:32 +0000

    message

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,8 +11,7 @@ int i, j;
 if (size > 0)
 {
 for (i = 0; i < size; i++)
-{
-for (j = 0; j < size; j++)
+{for (j = 0; j < size; j++)
 {
 if (j < size - i - 1)
 _putchar(' ');
@@ -22,4 +21,6 @@ _putchar('#');
 _putchar('\n');
 }
 }
+else
+_putchar('\n');
 }
