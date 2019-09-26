d1208fc774a66aecac49c4e2659e1412b4b18e2f
AhmedOmi <ahmedomarmiledi@gmail.com>
2019-09-26 14:20:53 +0000

    commit 0x04

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include"holberton.h"
+
+/**
+ * print_line- print line in the terminal
+ * @n: integer
+ * Return : void
+ */
+void print_line(int n)
+{
+int i;
+for (i = 0 ; i < n ; i++)
+{
+_putchar('_');
+}
+}
