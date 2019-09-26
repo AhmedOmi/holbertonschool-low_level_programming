7cf87eac148bef8fb3cabfebc64e2460db397b0b
AhmedOmi <ahmedomarmiledi@gmail.com>
2019-09-26 16:06:14 +0000

    commit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,6 +8,8 @@ void print_diagonal(int n)
 {
 int i;
 int j;
+if (n <= 0)
+_putchar('\n');
 
 for (i = 0; i < n; i++)
 {
@@ -25,6 +27,4 @@ _putchar('\\');
 }
 _putchar('\n');
 }
-if (n <= 0)
-_putchar('\n');
 }
