f36292c0740690a0cab5773393d21d0f5d314387
AhmedOmi <ahmedomarmiledi@gmail.com>
2019-10-01 06:21:08 +0000

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * swap_int - function to swipe to variable
+ * @a: a integer
+ * @b: b integer
+ * return : void
+ */
+void swap_int(int *a, int *b)
+{
+int tmp;
+tmp = *a;
+*a = *b ;
+*b = tmp;
+}
