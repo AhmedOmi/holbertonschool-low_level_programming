c2adf17a92328d663332864854865fdc6f0be85f
AhmedOmi <ahmedomarmiledi@gmail.com>
2019-10-01 21:58:48 +0000

    mesage

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main -  101keygen
+ *
+ * Return:  0
+ */
+int main(void)
+{
+ int count, count2, i;
+ char array[17] = "Ahmed omar miladi";
+ char pwd[100];
+srand(time(NULL));
+ i = 0;
+ count = 0;
+while (i < (2772 - 122))
+{
+	count2 = rand() % 62;
+	pwd[count] = array[count2];
+	i = i + pwd[count];
+	count++;
+}
+ count2 = 2772 - i;
+ pwd[count] = count2;
+ count++;
+ pwd[count] = '\0';
+ printf("%s", pwd);
+putchar('\n');
+return (0);
+}
