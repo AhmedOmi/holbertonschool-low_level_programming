8e8f05ee990372de4c30ad1a522ffa0d9d1ac011
AhmedOmi <ahmedomarmiledi@gmail.com>
2019-10-01 15:11:40 +0000

    message

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_rev - pointeur in reverser
+ * @s : ponter of char
+ * return : void
+ */
+void print_rev(char *s)
+{
+int i;
+int j;
+while (s[i] != '\0')
+{
+i++;
+}
+for (j = i; j >= 0; j--)
+_putchar(s[j]);
+_putchar('\n');
+
+}
--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,14 +3,19 @@
 /**
  * rev_string - reverser string
  * @s: char pointer
- * Return: Always 0.
+ * Return: Always 0
  */
 void rev_string(char *s)
 {
 int i = 0;
-while (s[i] != '\0')
-i++;
-for (; i >= 0; i--)
-_putchar(s[i]);
-_putchar('\n');
+int length = 0;
+char a;
+while (s[length] != '\0')
+length = length + 1;
+for (i = 0; i < length / 2; i++)
+{
+a = s[i];
+s[i] = s[length - i - 1];
+s[length - i - 1] = a;
+}
 }
