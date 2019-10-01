80f12b58ee22e63192bf77e8f79a590278b5e930
AhmedOmi <ahmedomarmiledi@gmail.com>
2019-10-01 15:35:08 +0000

    message

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * puts2 - putch
+ * @str: char str
+ * Return: void
+ */
+void puts2(char *str)
+{
+int l = 0;
+while(str[l] != '\0')
+{
+_putchar(str[l]);
+l = l + 2;
+}
+_putchar('\n');
+}
