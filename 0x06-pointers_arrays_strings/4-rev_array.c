f9ba99bfe9209b5635dccece89fc76abbb5c7cff
AhmedOmi <ahmedomarmiledi@gmail.com>
2019-10-03 19:55:42 +0000

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - check the code for Holberton School students.
+ * @a: pointer of integers
+ * @n: pointer of integer
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+int i;
+int j;
+int b;
+if (n % 2 != 0)
+b = n + 1;
+else
+b = n;
+for (i = 0; i < b / 2; i++)
+{
+j = a[i];
+a[i] = a[n - 1 - i];
+a[n - 1 - i] = j;
+}
+}
---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include"holberton.h"
+#include<stdio.h>
+/**
+ * string_toupper - fonction toupper
+ * @a : pointer char
+ * return : char
+ */
+char *string_toupper(char *a)
+{
+int i;
+for (i = 0; a[i] != 0; i++)
+{
+if (a[i] > 90 && a[i] != 10)
+{
+a[i] = a[i] - 32;
+}
+}
+return (a);
+}
