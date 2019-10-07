073e466ae5221860905851bb610f16f60c58c750
AhmedOmi <ahmedomarmiledi@gmail.com>
2019-10-07 20:01:44 +0000

    message

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,33 +1,82 @@
 #include"holberton.h"
 #include<stdio.h>
 /**
+ * _strlen - function to find the length of char or array
+ * @array: pointer of char
+ * Return: integer
+ */
+int _strlen(char *array)
+{
+int a = 0;
+
+while (*(array + a) != '\0')
+a++;
+return (a);
+}
+/**
+ *_strcmp - function compare
+ * @ch: char pointer
+ * @sh: char pointer
+ * Return: return int
+ */
+int _strcmp(char *ch, char *sh)
+{
+int a = 0;
+int b = 0;
+while (ch[a] != '\0' && sh[a] != '\0')
+{
+if (ch[a] > sh[a])
+return (ch[a] - sh[a]);
+else if (ch[a] < sh[a])
+return (-1 * (sh[a] - ch[a]));
+a++;
+}
+return (b);
+}
+/**
+ * _strncpy - function
+ * @a: pointer  char
+ * @b: pointer char
+ * @n: integer
+ * Return: char
+ */
+char *_strncpy(char *a, char *b, int n)
+{
+int i;
+
+for (i = 0; i < n && b[i] != '\0'; i++)
+{
+a[i] = b[i];
+}
+for ( ; i < n; i++)
+{
+a[i] = '\0';
+}
+return (a);
+}
+/**
 * _strstr - function .
 * @haystack: pointer of char
 * @needle: pointer of char
 * Return: return char
 */
-int _strlen(char *arr);
-int _strcmp(char *s1, char *s2);
-char *_strncpy(char *dest, char *src, int n);
 
 char *_strstr(char *haystack, char *needle)
 {
-char buffer[512];
+char a[512];
 int i = 0;
-int haystack_len = _strlen(haystack);
-int needle_len = _strlen(needle);
-
-while (i <= haystack_len - needle_len)
+int len1 = _strlen(haystack);
+int len2 = _strlen(needle);
+while (i <= len1 - len2)
 {
-_strncpy(buffer, haystack + i, needle_len);
-buffer[needle_len] = 0;
+_strncpy(a, haystack + i, len2);
+a[len2] = 0;
 
-if (_strcmp(buffer, needle) == 0)
+if (_strcmp(a, needle) == 0)
 {
 return (haystack + i);
 }
 i++;
 }
-
 return (0);
 }
