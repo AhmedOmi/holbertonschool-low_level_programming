c1003280e76f5ff7ed84f5a65e786b361c2b9664
AhmedOmi <ahmedomarmiledi@gmail.com>
2019-10-09 13:01:40 +0000

    0-puts

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include"holberton.h"
+#include<stdio.h>
+/**
+ * _puts_recursion - function puts recursion
+ * @s: pointer char
+ * Return: void.
+ */
+void _puts_recursion(char *s)
+{
+int i = 1;
+if (*s == '\0')
+{
+_putchar('\n');
+}
+else
+{
+_putchar(*s);
+_puts_recursion(s + i);
+i++;
+}
+
+}
