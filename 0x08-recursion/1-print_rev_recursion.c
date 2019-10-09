b0adc719dae98a9904ee27a6ad49b1c9ffc8a224
AhmedOmi <ahmedomarmiledi@gmail.com>
2019-10-09 13:25:30 +0000

    0-puts

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include"holberton.h"
+#include<stdio.h>
+/**
+ * _print_rev_recursion - function print reverse recursion.
+ * *s: char pointer
+ * Return: void.
+ */
+void _print_rev_recursion(char *s)
+{
+int i=0;
+if (s[i] == '\0')
+{
+_putchar('\n');
+}
+else
+{
+_putchar(
+}
