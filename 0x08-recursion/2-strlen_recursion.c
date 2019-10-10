7160f0b93def0812ed1233e0f26a9969f0b72353
AhmedOmi <ahmedomarmiledi@gmail.com>
2019-10-10 10:16:17 +0000

    message

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include"holberton.h"
+#include<stdio.h>
+/**
+ * _strlen_recursion - function print reverse recursion.
+ * @s: pointer of char
+ * Return: int
+ */
+int _strlen_recursion(char *s)
+{
+int n;
+if ( *s == '\0')
+return(0);
+s++;
+n = 1 + _strlen_recursion(s);
+return (n);
+}
