6304b1edc5345251acca42ca811ee5bf76c27b7a
AhmedOmi <ahmedomarmiledi@gmail.com>
2019-10-10 17:50:25 +0000

    message

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,8 +1,30 @@
-#include "holberton.h"
-#include <stdio.h>
+#include"holberton.h"
+#include<stdio.h>
+/**
+ * _prime - function
+ * @i: int
+ * @x: int
+ * Return: void
+ */
+int _prime(int i, int x)
+{
+if (x <= 1 || !(x % i))
+return (0);
+else if (i > x / 2)
+return (1);
+else
+return (_prime(i + 1, x));
+}
 
 /**
- * int is_prime_number - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * is_prime_number - function
+ * @n: integer
+ * Return: int
  */
+int is_prime_number(int n)
+{
+if (n == 2)
+return (1);
+else
+return (_prime(2, n));
+}
