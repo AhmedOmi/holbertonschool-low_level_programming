842af21df8259c8fcc58d44e86a5a9311cd7ac88
AhmedOmi <ahmedomarmiledi@gmail.com>
2019-10-13 19:58:44 +0000

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - function
+ * @argc: integer
+ * @argv: char pointer
+ * Return: int 0
+ */
+int main(int argc, char const *argv[] __attribute__((unused)))
+{
+printf("%d\n", argc - 1);
+return (0);
+}
