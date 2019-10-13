0a71cc885278e2439ec1f5072671284cd612938c
AhmedOmi <ahmedomarmiledi@gmail.com>
2019-10-13 19:48:53 +0000

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - funtion
+ * @argc: integer
+ * @argv: char pointer
+ * Return: int
+ */
+int main(int argc, char const *argv[])
+{
+printf("%s\n", *argv);
+return (0);
+}
