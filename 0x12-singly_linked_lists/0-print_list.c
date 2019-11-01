c6aedc63aab9d3af768a78727be6095fcd07b9c3
AhmedOmi <ahmedomarmiledi@gmail.com>
2019-11-01 09:45:24 +0100

    message

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,16 +7,13 @@
 size_t print_list(const list_t *h)
 {
 size_t n = 0;
-if (h->str == NULL)
-printf("[0] (nil)");
-else
-{
 while (h != NULL)
 {
+if (h->str == NULL)
+printf("[0] (nil)");
 printf("[%d] %s\n", h->len, h->str);
 h = h->next;
 n++;
-}
 }
 return (n);
 }
