a1ea41b3d325457e0c6019b59937b2d9d9b43646
AhmedOmi <ahmedomarmiledi@gmail.com>
2019-11-05 19:28:03 +0100

    message 8

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,14 +9,14 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-listint_t *temp = head;
-unsigned int count = 0;
-while (temp != NULL)
-{
+listint_t *temp = head;  
+unsigned int count = 0;  
+while (temp != NULL)  
+{  
 if (count == index)
-return (temp->next);
-count++;
-temp = temp->next;
-}
+return(temp->next);  
+count++;  
+temp = temp->next;  
+}  
 return (temp);
 }
---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * free_sum_listint - function to sum list
+ * @head: pointer of pointer struct
+ * Return: integer sum af all list
+ */
+int sum_listint(listint_t *head)
+{
+if(head)
+{
+int sum = 0;
+while (head)
+{
+sum += head->n;
+head = head->next;    
+}
+return (sum);
+}
+else{
+return (0);
+}
+}
