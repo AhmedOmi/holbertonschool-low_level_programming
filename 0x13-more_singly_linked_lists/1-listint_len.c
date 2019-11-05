82a9d368058ce918426933f3f42b06f4bc48e41a
AhmedOmi <ahmedomarmiledi@gmail.com>
2019-11-05 13:16:01 +0100

    message

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -9,12 +9,10 @@
  */
 size_t listint_len(const listint_t *h)
 {
-size_t i;
-const listint_t *tmp;
-tmp = head;
-while (tmp)
+size_t i = 0;
+while (h)
 {
-tmp = tmp->next;
+h = h->next;
 i++;
 }
 return (i);
