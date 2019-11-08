d0f263e8191cb3c357e9cbeb0140ddd2720ab5ba
AhmedOmi <ahmedomarmiledi@gmail.com>
2019-11-08 16:51:59 +0100

    message 0x14

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,11 +10,11 @@
  */
 void print_binary(unsigned long int n)
 {
-    if (n < 2)
-    putchar(n +'0');
-    while (n)
-        {
-        putchar ((n & 1) + '0');
-        n = (n >> 1);
-        }
+if (n < 2)
+putchar(n + '0');
+while (n)
+{
+putchar ((n & 1) + '0');
+n = (n >> 1);
+}
 }
