ff582be5b09fb23852bf95dd82e9a5641c82b6ec
ahmedomi <ahmedomarmiledi@gmail.com>
2020-01-19 15:44:39 +0000

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,6 +10,8 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 hash_table_t *tab;
+if (size == 0)
+return NULL;
 tab = malloc(sizeof(hash_table_t));
 tab->size = size;
 tab->array = malloc(sizeof(hash_node_t));
