524f59015b7a8b8a12a5b5a60aa219ae0fec6ae4
ahmedomi <ahmedomarmiledi@gmail.com>
2020-01-19 16:02:23 +0000

    create a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,8 +12,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 hash_table_t *tab;
 tab = malloc(sizeof(hash_table_t));
 if (tab == NULL)
-return NULL;
+return (NULL);
 tab->size = size;
 tab->array = malloc(sizeof(hash_node_t));
+if (tab->array == NULL)
+return (NULL);
 return (tab);
 }
