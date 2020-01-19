430de3e93c0b06d84cb312c0172a95b01e979abd
ahmedomi <ahmedomarmiledi@gmail.com>
2020-01-19 17:43:12 +0000

    function djb2

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+/**
+ * hash_djb2 - function djb2
+ * @str: const unsigned char
+ * Return: hash
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+unsigned long int hash;
+int c;
+hash = 5381;
+while ((c = *str++))
+{
+hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+}
+return (hash);
+}
