f29416baf687daa404869891f3ea5ffe77fcbbc5
ahmedomi <ahmedomarmiledi@gmail.com>
2020-01-25 17:36:17 +0000

    5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -9,16 +9,17 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-    unsigned long int i;
-    hash_node_t *node;
-    if (ht == NULL)
-    return ;
-    for (i = 0; i < ht->size; i++)
-	{
-		node = ht->array[i];
-		while (node)
-		{   printf("'%s': '%s'", node->key, node->value);
-			node = node->next;
-		}
-	}
+unsigned long int i;
+hash_node_t *node;
+if (ht == NULL)
+return;
+for (i = 0; i < ht->size; i++)
+{
+node = ht->array[i];
+while (node)
+{
+printf("'%s': '%s'", node->key, node->value);
+node = node->next;
+}
+}
 }
